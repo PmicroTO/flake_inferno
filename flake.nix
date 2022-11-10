93f01e3af30939a31b5d7c9f9d2befdd985b4de1
wnxkiv85 <8f2w79@getgoogleoff.me>
2022-11-10 06:56:05 -0300

    starting point nixflake inferno

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+	description = "my machine";
+	inputs = { 
+			nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+			home-manager = {
+				url = "github:nix-community/home-manager";
+				inputs.nixpkgs.follows = "nixpkgs";
+			};
+		
+		};
+
+  outputs = { self, nixpkgs, home-manager, ... }: 
+  
+	let
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+	in
+	
+  {
+    nixosConfigurations.inferno = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        [ ({ pkgs, ... }: {
+
+            	./base_system.nix
+		/.boot.nix
+		/.configuration.nix
+		/.gnome.nix
+		/.hardware-configuration.nix
+		/.networking.nix
+		/.pipewire_conf.nix
+		/.pipewire.nix
+		system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+          })
+        ];
+    };
+      homeConfigurations.lucio = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./home.nix
+        ];
+      };
+  };
+}
+
