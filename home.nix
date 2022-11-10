d58dcd1277c90e9b0bfe9fd93a74e9e2678324b6
wnxkiv85 <8f2w79@getgoogleoff.me>
2022-11-10 09:41:59 -0300

    test

--- home.nix
+++ home.nix
@@ -97,7 +97,7 @@
 			};
 		};
 		dconf.settings = {
-		"org/gnome/desktop/peripherals/trackball" = { scroll-wheel-emulation-button = 8; };
+		"org/gnome/desktop/peripherals/trackball" = { scroll-wheel-emulation-button = 9; };
 
 
 		
