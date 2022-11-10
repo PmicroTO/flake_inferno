180db6c3a442af3feb0455b28656adb84402dd3b
wnxkiv85 <8f2w79@getgoogleoff.me>
2022-11-10 09:19:10 -0300

    hm aliiases

--- home.nix
+++ home.nix
@@ -52,7 +52,6 @@
 			opvault = "ecryptfs-mount-private ; sleep 300 ; ecryptfs-umount-private";	
 			ls = "ls -A --color=auto -cltp --si --group-directories-first";			
 			flkhr = "home-manager switch --max-jobs 8 --flake /home/lucio/Projects/flake_inferno#lucio";
-			flkhu = "home-manager switch --max-jobs 8 --update-input /home/lucio/Projects/flake_inferno#lucio";
 			flkup = "sudo nixos-rebuild switch -j 8 --verbose --upgrade --flake /home/lucio/Projects/flake_inferno/";
 			flkre = "sudo nixos-rebuild switch -j 8 --verbose --flake /home/lucio/Projects/flake_inferno/";
 		};
