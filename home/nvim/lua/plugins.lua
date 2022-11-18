b1e7b27cf35968e47cfa738bc13945eb1fe753bc
wnxkiv85 <8f2w79@getgoogleoff.me>
2022-11-18 14:52:12 -0300

    nvim

--- home/nvim/lua/plugins.lua
+++ home/nvim/lua/plugins.lua
@@ -2,7 +2,7 @@ return require('packer').startup(function(use)
 
     use {
         "RRethy/nvim-base16",
-        config = "vim.cmd('colorscheme base16-gruvbox-dark-soft')"
+        config = "vim.cmd('colorscheme base16-tomorrow-night-eighties')"
     }
 
     use {
@@ -21,7 +21,45 @@ return require('packer').startup(function(use)
         }
     }
 
-use 'beauwilliams/statusline.lua'
+    use {
+        'nvim-lualine/lualine.nvim',
+        config = function()
 
+            require('lualine').setup {
+                options = {
+                    theme = base16,
+                    component_separators = '|',
+                    section_separators = {left = '', right = ''}
+                },
+                sections = {
+                    lualine_a = {
+                        {'mode', separator = {left = ''}, right_padding = 2}
+                    },
+                    lualine_b = {'filename', 'branch'},
+                    lualine_c = {'fileformat'},
+                    lualine_x = {},
+                    lualine_y = {'filetype', 'progress'},
+                    lualine_z = {
+                        {
+                            'location',
+                            separator = {right = ''},
+                            left_padding = 2
+                        }
+                    }
+                },
+                inactive_sections = {
+                    lualine_a = {'filename'},
+                    lualine_b = {},
+                    lualine_c = {},
+                    lualine_x = {},
+                    lualine_y = {},
+                    lualine_z = {'location'}
+                },
+                tabline = {},
+                extensions = {}
+            }
+        end,
+        requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    }
 
 end)
