fab1a064aebae4376360065dca483c8f3085147f
wnxkiv85 <8f2w79@getgoogleoff.me>
2022-11-20 23:24:12 -0300

    fixes fixes

--- home/nvim/lua/pconf/mason.lua
+++ home/nvim/lua/pconf/mason.lua
@@ -1,31 +1,29 @@
-    require("mason").setup()
-
+require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "pyright","rnix" },
+	ensure_installed = { "pyright", "rnix" },
 })
+require("mason-lspconfig").setup_handlers {
+	-- The first entry (without a key) will be the default handler
+	-- and will be called for each installed server that doesn't have
+	-- a dedicated handler.
+	function(server_name) -- default handler (optional)
+		require("lspconfig")[server_name].setup {}
+	end,
+}
+require 'lspconfig'.sumneko_lua.setup {
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT'
+			},
+			diagnostics = {
+				globals = { 'vim' }
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true)
+			},
+			telemetry = { enable = false }
+		}
+	}
 
-require'lspconfig'.bashls.setup{}
-require'lspconfig'.rnix.setup{}
-require'lspconfig'.pyright.setup{}
-
-    require'lspconfig'.sumneko_lua.setup {
- settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT'
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {'vim'}
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true)
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {enable = false}
-        }
-    }
-
-    }
+}
--- home/nvim/lua/plugins.lua
+++ home/nvim/lua/plugins.lua
@@ -1,57 +1,79 @@
 return require('packer').startup(function(use)
 
-    use {
-        "RRethy/nvim-base16",
-        config = "vim.cmd('colorscheme base16-tokyo-night-terminal-dark')"
-    }
+	use {
+		"RRethy/nvim-base16",
+		config = "vim.cmd('colorscheme base16-tokyo-night-terminal-dark')"
+	}
 
-    use {
-        {
-            "williamboman/mason.nvim",
+	use {
+		{
+			"williamboman/mason.nvim",
 
-            config = function() require('pconf.mason') end
+			config = function() require('pconf.mason') end
 
-        }, {"williamboman/mason-lspconfig.nvim"},
-{"neovim/nvim-lspconfig",
+		},
+
+		{ "williamboman/mason-lspconfig.nvim" },
+
+		{ "neovim/nvim-lspconfig",
+			requires = {
+				"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+				config = function()
+					require("lsp_lines").setup()
+					vim.diagnostic.config({ virtual_text = false })
+				end
+			} },
+
+		{ "jose-elias-alvarez/null-ls.nvim",
+			requires = "nvim-lua/plenary.nvim"
+
+		},
+
+		{ "jayp0521/mason-null-ls.nvim",
+			config = function()
+				require("mason").setup()
+				require("null-ls").setup()
+				require("mason-null-ls").setup({
+					ensure_installed = { "autopep8", "beautysh" },
+					automatic_setup = true
+				})
+				require 'mason-null-ls'.setup_handlers()
+			end
+		}
+
+	}
+
+	use {
+		'ojroques/nvim-hardline',
+		config = function() require('pconf.line') end
+
+	}
+
+	use { 'j-hui/fidget.nvim', config = function() require('pconf.fidget') end }
+
+	use {
+		'goolord/alpha-nvim',
+		requires = { 'kyazdani42/nvim-web-devicons' },
+		config = function() require 'alpha.themes.alexis12119_alpha' end
+	}
+
+	use {
+		'nvim-telescope/telescope.nvim',
+		config = function() require('pconf.telescope') end,
+		tag = '0.1.0',
 		requires = {
-                "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-                config = function()
-                    require("lsp_lines").setup()
-                    vim.diagnostic.config({virtual_text = false})
-                end
-            } }
-    }
-    use {
-        'ojroques/nvim-hardline',
-        config = function() require('pconf.line') end
+			{ 'nvim-lua/plenary.nvim' },
+			{ 'nvim-telescope/telescope-fzy-native.nvim' }
+		}
+	}
 
-    }
-
-    use {'j-hui/fidget.nvim', config = function() require('pconf.fidget') end}
-
-    use {
-        'goolord/alpha-nvim',
-        requires = {'kyazdani42/nvim-web-devicons'},
-        config = function() require 'alpha.themes.alexis12119_alpha' end
-    }
-
-    use {
-        'nvim-telescope/telescope.nvim',
-        config = function() require('pconf.telescope') end,
-        tag = '0.1.0',
-        requires = {
-            {'nvim-lua/plenary.nvim'},
-            {'nvim-telescope/telescope-fzy-native.nvim'}
-        }
-    }
-
-    use {
-        'nvim-tree/nvim-tree.lua',
-        config = function() require('pconf.nvim-tree') end,
-        requires = {
-            'nvim-tree/nvim-web-devicons' -- optional, for file icons
-        },
-        tag = 'nightly' -- optional, updated every week. (see issue #1193)
-    }
+	use {
+		'nvim-tree/nvim-tree.lua',
+		config = function() require('pconf.nvim-tree') end,
+		requires = {
+			'nvim-tree/nvim-web-devicons'
+		},
+		tag = 'nightly'
+	}
 
 end)
