15cc1df8e14b69b613c7c2d34e2d6e3d58c3fbda
wnxkiv85 <8f2w79@getgoogleoff.me>
2022-11-27 10:09:21 -0300

    force monitor refresh rate

--- boot.nix
+++ boot.nix
@@ -12,7 +12,7 @@
       verbose = false;
     };
     extraModprobeConfig = " options v4l2loopback exclusive_caps=1 ";
-    kernelParams = [ "amd_iommu=off" "iommu=soft" "quiet" "loglevel=3" ];
+    kernelParams = [ "amd_iommu=off" "iommu=soft" "quiet" "loglevel=3" "video=HDMI-A-1:2560x1080@75" ];
     consoleLogLevel = 0;
     loader.timeout = 1;
     kernelPackages = pkgs.linuxPackages_zen;
