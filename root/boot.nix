26b2bb9059d0dbb128aa0c23ad02bbd5ce48efc7
wnxkiv85 <8f2w79@getgoogleoff.me>
2022-12-10 08:38:31 -0300

    v4l2 name

--- root/boot.nix
+++ root/boot.nix
@@ -11,7 +11,7 @@
       systemd.enable = true;
       verbose = false;
     };
-    extraModprobeConfig = " options v4l2loopback exclusive_caps=1 ";
+    extraModprobeConfig = " options v4l2loopback card_label="Webcam" exclusive_caps=1 ";
     kernelParams = [ "amd_iommu=off" "iommu=soft" "quiet" "loglevel=3" "video=HDMI-A-1:2560x1080@75" ];
     consoleLogLevel = 0;
     loader.timeout = 1;
