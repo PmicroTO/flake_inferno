181f0f2826f60baa14df53b6ca89863bb29f35ad
wnxkiv85 <lucio_a.khfzo@aleeas.com>
2022-12-12 04:18:45 -0300

    tomb location

--- home/home-shell.nix
+++ home/home-shell.nix
@@ -27,7 +27,7 @@
     wp = "wgetpaste";
     ".." = "cd ..";
     #	webmloo = "for i in *.webm; do ffmpeg -stream_loop -1 -t 5 -i \"$i\" -c copy \"\${i%.*}_5.webm\"; done";
-    to = "tomb open $HOME/personal.tomb -k $HOME/personal.tomb.key ; sleep 600 ; tomb slam all";
+    to = "tomb open $HOME/tomb/personal.tomb -k $HOME/tomb/personal.tomb.key ; sleep 600 ; tomb slam all";
     slam = "tomb slam all";
   };
 }
