f097d20e7e7e8ded30b6401e84aff555cf550b5a
wnxkiv85 <lucio_a.khfzo@aleeas.com>
2022-12-13 10:16:20 -0300

    change locales

--- root/base_system.nix
+++ root/base_system.nix
@@ -14,19 +14,7 @@
   ];
 
   time.timeZone = "America/Fortaleza";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_CTYPE = "pt_BR.UTF-8";
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
-  };
+  i18n.defaultLocale = "en_GB.utf8";
 
   users.users.lucio = {
     isNormalUser = true;
